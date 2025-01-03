62a8261c763fb9c2463483c8b0592f93c4cab0e0
TogiFerretFerret <114247361+TogiFerretFerret@users.noreply.github.com>
2025-01-03 16:44:26 +0000

    test

--- bromine/t.py
+++ bromine/t.py
@@ -3,4 +3,13 @@ import apiwrapper
 import authwrapper
 print(googleauth.goog_murl("hudsonreich"))
 das=testspy.goog_megaauth()
-print(das)
+v=authwrapper.BbAuthWrapper("hudsonreich")
+v.goog_code(das)
+v.get_authsvctoken()
+v.get_bearer_token()
+tt=wrap.get_tt()
+print(tt)
+api=apiwrapper.BbApiWrapper(tt)
+api.get_uid()
+with open("example.json", "w") as f:
+    f.write(str(api.rawresult))
