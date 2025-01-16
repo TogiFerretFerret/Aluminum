bc0ca8bffdd6391fbe4f1eec16c8b1b28d3f88f9
TogiFerretFerret <114247361+TogiFerretFerret@users.noreply.github.com>
2025-01-16 11:37:08 -0500

    Update setupandrun.bat

--- setupandrun.bat
+++ setupandrun.bat
@@ -8,16 +8,15 @@ mkdir python
 tar -xf python-3.10.0-embed-amd64.zip -C python
 
 REM Add Python to PATH
-set PATH=%CD%\python;%PATH%
-
+set PATH=%CD%\python;%CD%\python\scripts;%PATH%
 REM Install pip
+python\python.exe pipfix.py
 echo Installing pip...
 curl -LO https://bootstrap.pypa.io/get-pip.py
 python\python.exe get-pip.py
-
 REM Install packages from requirements.txt
 echo Installing required packages...
-python\python.exe -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 REM Open two command windows to run lms.py and run-uv.py
 echo Starting lms.py and run-uv.py in separate command windows...
