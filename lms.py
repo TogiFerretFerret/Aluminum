721ddefe811d838bd50ed7d160d3930cbaabb713
TogiFerretFerret <114247361+TogiFerretFerret@users.noreply.github.com>
2025-01-16 11:37:36 -0500

    Update lms.py

--- lms.py
+++ lms.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import bromine.apiwrapper as apiwrapper
 import bromine.authwrapper as authwrapper
 import bromine.googleauth as googleauth
